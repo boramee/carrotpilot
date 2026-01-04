04bc7f262965e496def83e413cc0da2e16ed79ff
boramee <boramee0131@gmail.com>
2026-01-04 12:45:17 +0900

    Update carcontroller.py

--- opendbc_repo/opendbc/car/hyundai/carcontroller.py
+++ opendbc_repo/opendbc/car/hyundai/carcontroller.py
@@ -330,7 +330,7 @@ class CarController(CarControllerBase):
         can_sends.extend(hyundaicanfd.create_suppress_lfa(self.packer, self.CAN, CS))
 
       # LFA and HDA icons
-      if self.frame % 5 == 0 and camera_scc:
+      if self.frame % 5 == 0 :
         can_sends.extend(hyundaicanfd.create_lfahda_cluster(self.packer, CS, self.CAN, CC.longActive, CC.latActive))
 
       # blinkers
